3cbf64e3c933ffd4b657c51d977267cbb101c2ab
shifu012 <taha.nasa000@gmail.com>
2024-11-05 10:51:51 +0330

    Add files via upload

---
+++ script_py.py
@@ -0,0 +1,32 @@
+from os import system
+from colorama import Fore
+
+system ("clear")
+
+print ("hello world")
+print ("the script filtring_error")
+print ("sakhte shode tavasot gorooh hiroshima")
+code = input (f"""
+        
+              (1) bug
+              (2) algoritm
+              (3) link sexy
+              (4) matn filter account
+              (5) link mokhareb      
+              """)
+
+
+
+if code == "1":
+ print("online.ofline.com")
+if code == "2":
+ print("https://1.0.0.4.3.8.8.8.7.2.2.3.1.0.0.0.0.9.9.9.9.9.3.4.4.4.4.0.1.4.7.9.8.spam.ir")
+        
+
+if code == "3":
+ print ("www.pornhub.com")
+if code == "4":
+ print ("This account is dangerous in every way, please, this account is like hacker or terrorist accounts")
+if code == "5":
+ print("https://darkweb-sites.org/?s=https%3A%2F%2Ffiltering-rubika.com")
+0
